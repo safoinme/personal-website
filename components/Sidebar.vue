0834cca09c407fe26ff461013f196e4580ce9cf4
Hamza Mogni <hamzamogni5@gmail.com>
2022-04-17 17:28:31 +0000

    add personal avatar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row class="flex-column" align="center">
-    <v-avatar class="my-4">
-      <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+    <v-avatar class="my-4" size="150">
+      <img src="~assets/hmogni.jpg" alt="John" />
     </v-avatar>
     <p class="text-h6 mb-0">Hamza Mogni</p>
     <p class="subtitle-1 mt-0">Software Engineer</p>
