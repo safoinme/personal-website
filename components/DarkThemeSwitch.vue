aa546bf8bca228dc9b0e655956d4b32a4f18b613
Hamza Mogni <hamzamogni5@gmail.com>
2022-05-29 19:59:28 +0100

    add dark theme switch

---
+++ components/DarkThemeSwitch.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-btn icon x-large @click.prevent="switchTheme">
+    <v-icon>{{ icon }}</v-icon>
+  </v-btn>
+</template>
+
+<script>
+import { mdiWeatherNight, mdiWhiteBalanceSunny } from "@mdi/js";
+export default {
+  computed: {
+    icon: function () {
+      return this.$vuetify.theme.dark ? mdiWhiteBalanceSunny : mdiWeatherNight;
+    },
+  },
+
+  methods: {
+    switchTheme() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    },
+  },
+};
+</script>
+
+<style></style>
